ca555e932e12d82df4f7f36583b68f563b8ca10d
Ben Livermore <blivermore@wespire.com>
2016-11-08 11:06:47 -0500

    Update slacker.js

--- slacker.js
+++ slacker.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
@@ -14,27 +15,40 @@ app.post('/tp/ticket', function (req, res) {
   const args = req.body.text;
   if (command === '/ticket') {
 
-    const entity = args.trim();
-    const token = 'MTpDQ3ZyK0I5WFpHeXdEK3hvZHJEU0xmeDFFSUs1NU5WM3JOVXZmSXQ5L2FzPQ==';
+    const text = args.trim();
 
-    return request(`https://intellifylearning.tpondemand.com/api/v1/UserStories/${entity}?access_token=${token}&format=json`, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        const ticket = JSON.parse(body);
+    let responseText;
+    if (!isNaN(text)) {
+      responseText = `https://intellifylearning.tpondemand.com/entity/${text}`;
+      return request(`https://intellifylearning.tpondemand.com/api/v1/UserStories/${text}?access_token=${tpToken}&format=json`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const ticket = JSON.parse(body);
+          
+          const message = 
+          `${ticket.Name}
+  Status: ${ticket.EntityState.Name}`
+          return res.json({
+            response_type: 'in_channel',
+            text: responseText,
+            attachments: [{
+              text: message
+            }]
+          });
+        } else {
+          return res.json({});
+        }
+      });
+    } else {
+      responseText = text.replace(/#[0-9]*/, (ticketDisplay) => {
+        const entity = ticketDisplay.substr(1);
+        return `<https://intellifylearning.tpondemand.com/entity/${entity}|${ticketDisplay}>`
+      });
+      return res.json({
+        response_type: 'in_channel',
+        text: responseText
+      })
+    }
 
-        const message = 
-        `${ticket.Name}
-Status: ${ticket.EntityState.Name}`
-        return res.json({
-          response_type: 'in_channel',
-          text: `https://intellifylearning.tpondemand.com/entity/${entity}`,
-          attachments: [{
-            text: message
-          }]
-        });
-      } else {
-        return res.json({});
-      }
-    });
   }
 
   return res.json({});
